eb5dd48b898e012d31c8f557a50c07923ae46bdc
Svetlana Kutyova <kutyovas@yandex.ru>
2021-11-30 23:18:35 +0300

    2

--- src/js/__tests__/GameSavingLoader.test.js
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -4,13 +4,3 @@ test('the data is peanut butter', async () => {
   const data = await GameSavingLoader.load();
   expect(data).toBe('{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}');
 });
-/*
-test('the fetch fails with an error', async () => {
-  expect.assertions(0);
-  try {
-    await GameSavingLoader.load();
-  } catch (e) {
-    expect(e).toMatch('error');
-  }
-});
-*/
--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,7 +1,6 @@
 import GameSavingLoader from '../GameSavingLoader.js';
 
-test('the data is saving', () => {
-  GameSavingLoader.load().then((saving) => {
-    expect(saving).toEqual('{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}');
-  });
+test('the data is peanut butter', async () => {
+  const data = await GameSavingLoader.load();
+  expect(data).toBe('{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}');
 });
