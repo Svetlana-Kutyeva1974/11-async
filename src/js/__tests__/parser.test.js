7d4b996d27c05ec94a99f5f4f97fd3b5a83a1141
Svetlana Kutyova <kutyovas@yandex.ru>
2021-12-02 21:50:49 +0300

    2

--- src/js/__tests__/parser.test.js
+++ src/js/__tests__/parser.test.js
@@ -1,9 +1,8 @@
 import json from '../parser.js';
 
-test('the data is parser', () => {
+test('the data is parser', async () => {
   const data = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
   const str = String.fromCharCode.apply(null, new Uint16Array(data));
-  return json(data).then((response) => {
-    expect(response).toEqual(str);
-  });
+  const value = await json(data);
+  expect(value).toEqual(str);
 });
--- src/js/__tests__/reader.test.js
+++ src/js/__tests__/reader.test.js
@@ -1,9 +1,8 @@
 import read from '../reader.js';
 
-test('the data is buffer', () => {
+test('the data is buffer', async () => {
   const data1 = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
   const buffer = new ArrayBuffer(data1.length * 2);
-  return read().then((data) => {
-    expect(data).toEqual(buffer);
-  });
+  const data = await read();
+  expect(data).toEqual(buffer);
 });
