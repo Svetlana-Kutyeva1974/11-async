03aa89d354723dbc85a8e4593f2f74bac99aa2bf
Svetlana Kutyova <kutyovas@yandex.ru>
2021-12-02 21:40:56 +0300

    2

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,5 @@
 import GameSavingLoader from './GameSavingLoader.js';
 /*
-GameSavingLoader.load().then((saving) => saving, (error) => error);
-*/
-/*
 (async () => {
   let i = await GameSavingLoader.load();
 })();
@@ -10,9 +7,8 @@ GameSavingLoader.load().then((saving) => saving, (error) => error);
 (async () => {
   try {
     const promisres = await GameSavingLoader.load();
-    console.log('promisres :\n', promisres);
     return promisres;
   } catch {
-    console.log('error');
+    return new Error('error');
   }
 })();
